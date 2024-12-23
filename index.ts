f01487afd4f6fe427e6c3d446ce34ecbd0100d2e
meimanbaev <meinmanbaev.t@km-spb.com>
2024-12-23 13:43:03 +0300

    Добавлена тестовая страница

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { Elysia } from 'elysia'
 
 const app = new Elysia()
     .get('/', 'Hello World')
+    .get('/test', 'Test Page')
     .listen(3000);
     
 console.log(`Server is running at on port ${app.server?.port}`)
